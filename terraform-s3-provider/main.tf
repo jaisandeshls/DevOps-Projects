0b8af128c586788223006c428159264b645c73ce
jaisandeshls <demoemail404@gmail.com>
2022-07-02 22:21:29 +0530

    revert back

--- terraform-s3-provider/main.tf
+++ terraform-s3-provider/main.tf
@@ -1,17 +1,27 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.12.1"
-    }
-  }
-backend "s3" {
-    bucket = "terraform-state-files-443"
-    key    = "state-file/tfstate"
-    region = "us-east-1"
-  }
-}
 provider "aws" {
-  # Configuration options
   region = "us-east-1"
+
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+
+    #give bucket name unique
+    bucket = "terraform-statefiles-4341"
+    
+    versioning_configuration {
+    status = "Enabled"
+
+    }
+  
+}
+
+terraform{
+
+    backend "s3" {
+
+        bucket = "terraform-statefiles-4341"
+        key = "state-files/tfstate"
+        region = "us-east-1"
+
+    }
 }
